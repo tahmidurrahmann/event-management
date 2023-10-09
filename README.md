38f786862c6e219e53b055bf69ffd3fc6b5020eb
tahmidurrahmann <138325180+tahmidurrahmann@users.noreply.github.com>
2023-10-09 15:42:05 +0600

    added half of readme.md file

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-# React + Vite
+# Dream Eve
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+## [ Private Repo Link](https://social-event-management-c2413.web.app/)
 
-Currently, two official plugins are available:
+Click here for the private repo: [https://social-event-management-c2413.web.app/](https://social-event-management-c2413.web.app/)
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
--- src/pages/Banner/Banner.jsx
+++ src/pages/Banner/Banner.jsx
@@ -1,12 +1,12 @@
 const Banner = () => {
     return (
-            <div className="flex flex-col md:my-10 lg:my-24 md:flex-row p-16 md:p-8 lg:p-0 gap-10 md:gap-20 justify-center items-center" data-aos="fade-down" data-aos-duration="3000">
-                <div className="space-y-5">
+            <div className="flex flex-col md:my-10 lg:my-24 md:flex-row p-16 md:p-8 lg:p-0 gap-10 md:gap-20 justify-center items-center">
+                <div className="space-y-5" data-aos="fade-right" data-aos-duration="2000">
                     <h1 className="text-[#1C1B1B] text-4xl md:text-5xl lg:text-6xl font-extrabold">Organization of <span className="text-[#F9A51A]">Birthday Events</span></h1>
                     <p className="text-xl font-medium text-[#1C1B1B99]">We do not just plan birthdays, we craft experiences that will be cherished for a lifetime. Our mission is to transform your vision into a captivating reality.With meticulous attention to detail, a creative flair, and a passion for excellence, we curate celebrations that are nothing short of extraordinary.</p>
                     <button className="font-amatic text-2xl py-3 px-7 rounded-lg text-white font-medium bg-[#F9A51A]">WHAT WE OFFER</button>
                 </div>
-                <img className="w-full md:w-[50%] lg:w-[40%]" src="https://i.ibb.co/fNBYP8Y/customer-service.png" alt="" />
+                <img className="w-full md:w-[50%] lg:w-[40%]" src="https://i.ibb.co/fNBYP8Y/customer-service.png" alt="" data-aos="fade-left" data-aos-duration="2000"/>
             </div>
     );
 };
